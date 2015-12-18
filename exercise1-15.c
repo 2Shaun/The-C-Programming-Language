2201dbb8a23962a160b38843c1a2545edb629183
tomoshaughnessy3492@gmail.com <tommy@Tommy-PC.home>
2015-12-17 21:38:49 -0500

    Exercise 15 finished.

--- exercise1-15.c
+++ exercise1-15.c
@@ -2,26 +2,20 @@
 
 /* print Fahrenheit-Celsius table
  * for fahr = 0. 20, ..., 300 */
-int fahrToCelsius(int fahr, int upper);
+void fahrToCelsius(int fahr, int upper);
 
-main ()
+int main ()
 {
-	int fahr, celsius;
-	int lower, upper, step;
-
-	lower = 0;	/* lower limit of temperature table */
-	upper = 300;	/* upper limit */
-	step = 20;	/* step size */
-
-	fahr = lower;
-	while (fahr <= upper) {
-		celsius = 5 * (fahr-32)	/ 9;
-		printf("%d\t%d\n", fahr, celsius);
-		fahr = fahr + step;
-	}
+	fahrToCelsius(0, 100);
+	return 0;
 }
 
-int fahrToCelsius(int fahr, int upper)
+void fahrToCelsius(int fahr, int upper)
 {
+	while (fahr <= upper) {
+		int celsius = 5 * (fahr-32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr += 20;
+	}
 
 }
